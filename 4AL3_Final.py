980afb74020e6601ed523c2cd409b600b8484240
jmann6119 <jmann6119@gmail.com>
2025-11-02 17:42:00 -0500

    Nov 2 - 5:41 Main File update

--- 4AL3_Final.py
+++ 4AL3_Final.py
@@ -31,44 +31,47 @@ class my_svm():
         self.y = self.y[mask]
         '''
 
+        '''
         #Call standardscaler for normalization
         scalar = StandardScaler()
 
         #Preprocess self.x and rewrite to same variable
         self.x = scalar.fit_transform(self.x)
 
+        '''
+        scalar = StandardScaler()
+        self.x = scalar.fit_transform(self.x)
         return self.x, self.y
     
     def cross_validation(self, X, y):
-        kf = KFold(n_splits=10, shuffle=True, random_state=42)
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
         tss_scores = []
-
+        
         for train_idx, test_idx in kf.split(X):
+            
             X_train, X_test = X[train_idx], X[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
-
+            print('hi')
             model = self.training(X_train, y_train)
 
             y_pred = model.predict(X_test)
-
+            print(y_train)
             tss_score = self.tss(y_test, y_pred)
             tss_scores.append(tss_score)
-
 
         avg_tss = sum(tss_scores) / len(tss_scores)
         return avg_tss
     
     def training(self, X_train, y_train):
-        model = SVC(kernel="rbf", C = 1, gamma = 'scale')
+        model = SVC(kernel="linear", C = 1)
         model.fit(X_train, y_train)
-
         return model
 
     def tss(self, y_true, y_pred):
         TP = sum((y_true == 1) & (y_pred == 1))
         TN = sum((y_true == 0) & (y_pred == 0))
         FP = sum((y_true == 0) & (y_pred == 1))
-        FN = sum((y_true == 0) & (y_pred == 0))
+        FN = sum((y_true == 1) & (y_pred == 0))
 
         if TP + FN > 0:
             true_positive = TP/(TP + FN)
@@ -88,17 +91,20 @@ def experiment():
     diabetes_data = pd.read_csv("diabetes_binary_health_indicators_BRFSS2015.csv")
     ddf = diabetes_data.drop(columns=["CholCheck","AnyHealthcare","NoDocbcCost","GenHlth","MentHlth","PhysHlth","DiffWalk","Education", "Income"])
 
-    y_vals = ddf['Diabetes_binary']
+    y_vals = ddf['Diabetes_binary'].values
     features = ['HighBP', 'HighChol', 'BMI', 'Smoker', 'Stroke', 'HeartDiseaseorAttack', 'PhysActivity', 'Fruits', 'Veggies', 'HvyAlcoholConsump', 'Sex', 'Age']
-    X_vals = [ddf[name].values for name in features]
+    X_vals = np.column_stack([ddf[name].values for name in features])
 
     svm_model = my_svm(X_vals, y_vals)
     X_preprocessed, y_preprocessed = svm_model.preprocess()
+    print("Shape of X:", X_preprocessed.shape)
+    print("Shape of y:", y_preprocessed.shape)
+    print("First 5 labels:", y_preprocessed[:5])
     
     avg_tss = svm_model.cross_validation(X_preprocessed, y_preprocessed)
 
-    print("X_pre: ",X_preprocessed)
-    print("Y_pre: ",y_preprocessed)
+    #print("X_pre: ",X_preprocessed)
+    #print("Y_pre: ",y_preprocessed)
 
     print("avg_tss: ", avg_tss)
     
