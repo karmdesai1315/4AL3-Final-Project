6b18798102592132a84175b6798a42dedf310c2f
jmann6119 <jmann6119@gmail.com>
2025-11-02 16:27:32 -0500

    Nov 2, 4:27 Main file update

--- 4AL3_Final.py
+++ 4AL3_Final.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 import random
-
+'''
 class my_svm():
     def __init__(self, x_:list, y_:list, data_set):
         
@@ -132,6 +132,45 @@ class my_svm():
         ###########
         return skill_score
     
+'''
+
+class my_svm():
+    def __init__(self, x_:list, y_:list):
+        #Class features and labels initialization
+        self.x = np.array(x_)
+        self.y = np.array(y_)
+
+        #p to the ass
+        pass
+
+    def preprocess(self):
+        #Removing nan values from CSV file if they exist
+        '''
+        mask = ~np.isnan(self.x).any(axis=1) & ~np.isnan(self.y)
+        self.x = self.x[mask]
+        self.y = self.y[mask]
+        '''
+
+        #Call standardscaler for normalization
+        scalar = StandardScaler()
+
+        #Preprocess self.x and rewrite to same variable
+        self.x = scalar.fit_transform(self.x)
+
+        return self.x, self.y
 
 def experiment():
-    var = ''
+    diabetes_data = pd.read_csv("diabetes_binary_health_indicators_BRFSS2015.csv")
+    ddf = diabetes_data.drop(columns=["CholCheck","AnyHealthcare","NoDocbcCost","GenHlth","MentHlth","PhysHlth","DiffWalk","Education", "Income"])
+
+    y_vals = ddf['Diabetes_binary']
+    features = ['HighBP', 'HighChol', 'BMI', 'Smoker', 'Stroke', 'HeartDiseaseorAttack', 'PhysActivity', 'Fruits', 'Veggies', 'HvyAlcoholConsump', 'Sex', 'Age']
+    X_vals = [ddf[name].values for name in features]
+
+    svm_model = my_svm(X_vals, y_vals)
+    X_preprocessed, y_preprocessed = svm_model.preprocess()
+
+    print("X_pre: ",X_preprocessed)
+    print("Y_pre: ",y_preprocessed)
+
+experiment()
